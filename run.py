eec5376e5ff6be8f9ec28e51eb573e5932e270bf
David Lai <3357009+davidlatwe@users.noreply.github.com>
2018-08-16 20:25:40 +0800

    Update run.py

--- run.py
+++ run.py
@@ -2,5 +2,9 @@ from maya import standalone, cmds
 standalone.initialize()
 
 cmds.file(new=True, force=True)
-cube, _ = cmds.polyCube(name="testCube")
-print(cube)
+
+# This command failed in Maya 2017 +
+cmds.workspace("scene", query=True, fileRuleEntry=True)
+
+# This works well instead.
+# mel.eval("workspace -q -fileRuleEntry \"scene\"")
