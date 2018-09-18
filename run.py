31392cca1df09a750e09052030ee636c62ec79a1
David Lai <3357009+davidlatwe@users.noreply.github.com>
2018-09-18 18:44:20 +0800

    Update run.py

--- run.py
+++ run.py
@@ -3,8 +3,5 @@ standalone.initialize()
 
 cmds.file(new=True, force=True)
 
-# This command failed in Maya 2017 +
-cmds.workspace("scene", query=True, fileRuleEntry=True)
-
-# This works well instead.
-# mel.eval("workspace -q -fileRuleEntry \"scene\"")
+# Should work in all version
+print(cmds.workspace(fileRuleEntry="scene"))
