2e5bb0c8aeed8a75ee41148a2e288b9d083b1d10
David Lai <davidlatwe@gmail.com>
2018-11-13 22:51:34 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
+import os
 from maya import standalone, cmds
 standalone.initialize()
 
+os.environ["project"] = "/Path/To/My/Project"
+
 cmds.file(new=True, force=True)
-cube, _ = cmds.polyCube(name="testCube")
-print(cube)
+file_node = cmds.createNode("file")
+cmds.setAttr(file_node + ".fileTextureName",
+             "$project/img/pic.jpg",
+             type="string")
+file_path = cmds.getAttr(file_node + ".fileTextureName",
+                         expandEnvironmentVariables=True)
+
+assert file_path == "/Path/To/My/Project/img/pic.jpg"
